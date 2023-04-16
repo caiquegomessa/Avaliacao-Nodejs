e829599c4e5ecfc7fc144826a7af0aa0396f440f
caiquegomessa <caiquegomessa@gmail.com>
2023-04-16 01:29:33 -0300

    deletar aluno

--- Atividade/index.js
+++ Atividade/index.js
@@ -17,14 +17,25 @@ app.get("/alunos", (req, res) => {
 
 app.post("/alunos/novo", (req, res) => {
     const { nome, media, matricula } = req.query;
-    const novoAluno = { nome: nome, media: media, matricula: matricula};
+    const novoAluno = { nome: nome, media: media, matricula: matricula };
     if (nome !== undefined && media !== undefined && matricula !== undefined) {
         alunos.push(novoAluno)
         res.send(`Aluno adicionado com sucesso !`);
-      } else {
+    } else {
         res.status(400).send("Não foi possível adicionar o aluno.");
-      }
-  });
+    }
+});
+
+app.post("/alunos/deletar/:index", (req, res) => {
+    const { index } = req.params
+    if (index <= alunos.length - 1) {
+        alunos.splice(index, 1);
+        res.send(alunos)
+    } else {
+        res.status(404).send("Aluno não encontrado!")
+    }
+})
+
 
 
 
